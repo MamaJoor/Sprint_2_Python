2cfda55df077c58cbd00775beaa9991f6369067b
Ilia Mamaev <MamaJoor@yandex.ru>
2022-07-31 19:11:38 +0500

    Added tests and README

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# qa_python
+###test_dict_book_rating_exists 
+тестируется метод init, проверяется наличие пустого словаря books_rating
+
+###test_list_favorites_exists
+тестируется метод init, проверяется наличие пустого списка favorites
+
+###test_add_new_book_add_one_book
+тестируется метод add_new_book, проверяется успешность добавления одной книги 
+
+###test_add_new_book_add_two_books
+тестируется метод add_new_book, проверяется успешность добавления двух книг
+
+###test_add_new_book_add_two_same_books_only_one_added
+тестируется метод add_new_book, проверяется, что уже добавленная книга не добавится в список еще раз
+
+###test_add_new_book_add_one_book_rating_equals_1
+тестируется метод add_new_book, проверяется, что у добавленной книги по умолчанию рейтинг "1"
+
+###test_add_new_book_add_one_book_name_correct
+тестируется метод add_new_book, проверяется корректность названия добавленной книги 
+
+###test_set_book_rating_set_5_rating_updated
+тестируется метод set_book_rating, проверяется корректность изменения рейтинга 
+
+###test_set_book_rating_book_non_existent_dict_empty
+тестируется метод set_book_rating, проверяется, что нельзя изменить рейтинг книге, которой нет в словаре books_rating
+
+###test_set_book_rating_set_11_rating_still_1
+тестируется метод set_book_rating, проверяется, что нельзя выставить рейтинг выше 10
+
+###test_set_book_rating_set_negative_rating_still_1
+тестируется метод set_book_rating, проверяется, что нельзя выставить рейтинг ниже 1
+
+###test_get_book_rating_book_doesnt_exist
+тестируется метод get_book_rating, проверяется, что у книги, которой нет в словаре books_rating нет рейтинга 
+
+###test_get_books_with_specific_rating_1_book
+тестируется метод get_books_with_specific_rating, проверяется наличие одной книги в списке 
+
+###test_get_books_with_specific_rating_2_books_with_same_rating_2_books
+тестируется метод get_books_with_specific_rating, проверяется наличие нескольких книги в списке с одинаковы рейтингом 
+
+###test_get_books_with_specific_rating_2_of_the_3_books_with_same_rating_2_books
+тестируется метод get_books_with_specific_rating, проверяется, что выводятся книги только с переданным рейтингом 
+
+###test_get_books_with_specific_rating_books_with_same_updated_rating_2_books
+тестируется метод get_books_with_specific_rating, проверяется наличие нескольких книги в списке с одинаковы рейтингом, после изменения рейтинга
+
+###test_add_book_in_favorites_1_book_added
+тестируется метод add_book_in_favorites, проверяется добавления книги в список favorites
+
+###test_add_book_in_favorites_book_not_in_list_list_empty
+тестируется метод add_book_in_favorites, проверяется, что нельзя добавить книгу в список favorites, если ее нет в словаре books_rating
+
+###test_delete_book_from_favorites_book_in_list_book_deleted
+тестируется метод delete_book_from_favorites, проверяется удаление книги из списка favorites
+
+###test_delete_book_from_favorites_book_not_in_list
+тестируется метод delete_book_from_favorites, проверяется, что нельзя удалить книгу которой нет в словаре books_rating
--- tests.py
+++ tests.py
@@ -1,24 +1,162 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_dict_book_rating_exists(self):
         collector = BooksCollector()
 
-        # добавляем две книги
+        assert collector.get_books_rating() == {}
+
+    def test_list_favorites_exists(self):
+        collector = BooksCollector()
+
+        assert collector.get_list_of_favorites_books() == []
+
+    def test_add_new_book_add_one_book(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert len(collector.get_books_rating()) == 1
+
+    def test_add_new_book_add_two_books(self):
+        collector = BooksCollector()
+
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_two_same_books_only_one_added(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert len(collector.get_books_rating()) == 1
+
+    def test_add_new_book_add_one_book_rating_equals_1(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
+
+    def test_add_new_book_add_one_book_name_correct(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert 'Гордость и предубеждение и зомби' in collector.get_books_rating()
+
+    def test_set_book_rating_set_5_rating_updated(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+
+        assert collector.get_books_rating() == {'Гордость и предубеждение и зомби': 5}
+
+    def test_set_book_rating_book_non_existent_dict_empty(self):
+        collector = BooksCollector()
+
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+
+        assert collector.get_books_rating() == {}
+
+    def test_set_book_rating_set_11_rating_still_1(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        collector.set_book_rating('Гордость и предубеждение и зомби', 11)
+
+        assert collector.get_books_rating() == {'Гордость и предубеждение и зомби': 1}
+
+    def test_set_book_rating_set_negative_rating_still_1(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        collector.set_book_rating('Гордость и предубеждение и зомби', -5)
+
+        assert collector.get_books_rating() == {'Гордость и предубеждение и зомби': 1}
+
+    def test_get_book_rating_book_doesnt_exist(self):
+        collector = BooksCollector()
+
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == None
+
+    def test_get_books_with_specific_rating_1_book(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert len(collector.get_books_with_specific_rating(1)) == 1
+
+    def test_get_books_with_specific_rating_2_books_with_same_rating_2_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+
+        assert len(collector.get_books_with_specific_rating(1)) == 2
+
+    def test_get_books_with_specific_rating_2_of_the_3_books_with_same_rating_2_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_new_book('Фиолетовые тапки')
+
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+
+        assert len(collector.get_books_with_specific_rating(1)) == 2
+
+    def test_get_books_with_specific_rating_books_with_same_updated_rating_2_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+        collector.set_book_rating('Что делать, если ваш кот хочет вас убить', 5)
+
+        assert len(collector.get_books_with_specific_rating(5)) == 2
+
+    def test_add_book_in_favorites_1_book_added(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+
+        assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби']
+
+    def test_add_book_in_favorites_book_not_in_list_list_empty(self):
+        collector = BooksCollector()
+
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_delete_book_from_favorites_book_in_list_book_deleted(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+
+        collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
+
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_delete_book_from_favorites_book_not_in_list(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
+
+        assert len(collector.get_list_of_favorites_books()) == 0
